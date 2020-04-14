4d1eb1c4f809b28e9ea6bec13496e1d9f2d9369d
MaxMlV <max.malphil@gmail.com>
2020-04-14 18:48:25 +0300

    Number progression cleaned.

--- src/methods/numberProgression/NumberProgression.java
+++ src/methods/numberProgression/NumberProgression.java
@@ -10,85 +10,48 @@ public class NumberProgression {
         }
     }
 
-    public static int plusProgression(int[] num) {
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] + 2 == num[i+1] && i <= num.length-2) {
-                if (i == num.length-3) {
-                    num[num.length-1] = num[i+1] + 2;
-                    break;
-                } else {
-                    continue;
-                }
-            } else if (num[i] + 3 == num[i+1] && i <= num.length-2) {
-                if (i == num.length-3) {
-                    num[num.length-1] = num[i+1] + 3;
-                    break;
-                } else {
-                    continue;
-                }
-            } else {
-                break;
+    public static int calculatePlusProgression(int[] num) {
+        for (int i = 0; i < num.length - 1; i++) {
+            if (num[i] + 2 == num[i + 1] && i == num.length - 2) {
+                return num[i + 1] + 2;
+            } else if (num[i] + 3 == num[i + 1] && i == num.length - 2) {
+                return num[i + 1] + 3;
             }
         }
-        return num[num.length-1];
+        return 0;
     }
 
-    public static int multiplyProgression(int[] num) {
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] * 2 == num[i+1] && i <= num.length-2) {
-                if (i == num.length-3) {
-                    num[num.length-1] = num[i+1] * 2;
-                    break;
-                } else {
-                    continue;
-                }
-            } else if (num[i] * 3 == num[i+1] && i <= num.length-2) {
-                if (i == num.length-3) {
-                    num[num.length-1] = num[i+1] * 3;
-                    break;
-                } else {
-                    continue;
-                }
-            } else {
-                break;
+    public static int calculateMultiplyProgression(int[] num) {
+        for (int i = 0; i < num.length - 1; i++) {
+            if (num[i] * 2 == num[i + 1] && i == num.length - 2) {
+                return num[i + 1] * 2;
+            } else if (num[i] * 3 == num[i + 1] && i == num.length - 2) {
+                return num[i + 1] * 3;
             }
         }
-        return num[num.length-1];
+        return 0;
     }
 
-    public static int degreeProgression(int[] num) {
+    public static int calculatePowerProgression(int[] num) {
         for (int i = 1; i <= num.length; i++) {
-            if ((i * i) == num[i-1]) {
-                if (i == num.length-1) {
-                    num[i] = (i+1) * (i+1);
-                    break;
-                } else {
-                    continue;
-                }
-            } else if ((i * i * i) == num[i-1]) {
-                if (i == num.length-1) {
-                    num[i] = (i+1) * (i+1) * (i+1);
-                    break;
-                } else {
-                    continue;
-                }
-            } else {
-                break;
+            if ((i * i) == num[i - 1] && i == num.length - 1) {
+                return (int) Math.pow(i + 1, 2);
+            } else if ((i * i * i) == num[i - 1] && i == num.length - 1) {
+                return (int) Math.pow(i + 1, 3);
             }
         }
-        return num[num.length-1];
+        return 0;
     }
-
 
 
     public static int whichProgression(int[] num, String text) {
         int solution = 0;
-        if (plusProgression(num) > 0) {
-            solution = plusProgression(num);
-        } else if (multiplyProgression(num) > 0) {
-            solution = multiplyProgression(num);
-        } else if (degreeProgression(num) > 0) {
-            solution = degreeProgression(num);
+        if (calculatePlusProgression(num) > 0) {
+            solution = calculatePlusProgression(num);
+        } else if (calculateMultiplyProgression(num) > 0) {
+            solution = calculateMultiplyProgression(num);
+        } else if (calculatePowerProgression(num) > 0) {
+            solution = calculatePowerProgression(num);
         } else {
             solution = -1;
         }
@@ -101,8 +64,8 @@ public class NumberProgression {
         System.out.println("Enter number progression.");
         String strNumbers = scanner.nextLine();
         String[] clearStr = strNumbers.split(",");
-        int[] numbers = new int[clearStr.length+1];
+        int[] numbers = new int[clearStr.length];
         parseInt(clearStr, numbers);
-        System.out.println(whichProgression(numbers,"If you see number '-1', it means progression is incorrect."));
+        System.out.println(whichProgression(numbers, "If you see number '-1', it means progression is incorrect."));
     }
 }

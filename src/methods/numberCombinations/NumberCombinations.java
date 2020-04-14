fb243e3a3c34fe337a696b9e776f0e5041e88a11
MaxMlV <max.malphil@gmail.com>
2020-04-14 22:16:56 +0300

    NumberCombinations is done.

--- src/methods/numberCombinations/NumberCombinations.java
+++ src/methods/numberCombinations/NumberCombinations.java
@@ -1,7 +1,22 @@
 package methods.numberCombinations;
 
-public class NumberCombinations {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class NumberCombinations {
+
+    public static void calculateCombinations(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                System.out.println(Arrays.toString(array));
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        calculateCombinations(numbers);
     }
 }

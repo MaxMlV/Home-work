857f9934234dd950671d99d14fc9d2fe376a5599
MaxMlV <max.malphil@gmail.com>
2020-04-16 16:02:55 +0300

    SaveArray is done.

--- src/filesLecture/saveArray/SaveArray.java
+++ src/filesLecture/saveArray/SaveArray.java
@@ -1,7 +1,36 @@
 package filesLecture.saveArray;
 
-public class SaveArray {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class SaveArray {
+
+    public static void saveArray(String text, File file) {
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            printWriter.print(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String fromArrayToString(int[][] array) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int[] rows : array) {
+            for (int col : rows) {
+                stringBuilder.append(String.format("%-6.6s\t", "" + col));
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        int[][] numbers = new int[6][6];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                numbers[i][j] = j+1;
+            }
+        }
+        File file = new File("src/filesLecture/saveArray/saveArray.txt");
+        saveArray(fromArrayToString(numbers), file);
     }
 }

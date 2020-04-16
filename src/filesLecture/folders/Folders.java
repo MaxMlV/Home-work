92392ca0720a00ff64c9ea8cce83f05df90f8fcd
MaxMlV <max.malphil@gmail.com>
2020-04-16 16:19:21 +0300

    Folders is done.

--- src/filesLecture/folders/Folders.java
+++ src/filesLecture/folders/Folders.java
@@ -1,7 +1,20 @@
 package filesLecture.folders;
 
-public class Folders {
-    public static void main(String[] args) {
+import java.io.File;
 
+public class Folders {
+
+    public static void foldersOutput(File folder) {
+        File[] folders = folder.listFiles();
+        for (File folderElem : folders) {
+            if (!folderElem.isFile()) {
+                System.out.println(folderElem.getName());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File folder = new File("src/filesLecture/folders");
+        foldersOutput(folder);
     }
 }
